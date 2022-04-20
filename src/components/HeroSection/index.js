23b7114fd472678b5d3d6bc3e65f1f57dfcf38a9
Rishav Chnada <rishavchanda0@gmail.com>
2022-04-20 18:21:40 +0530

    made some changes

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, Title, SubTitle, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/hero3.jpg'
+import HeroImg from '../../images/HeroImage.jpg'
 const HeroSection = () => {
     return (
         <>
