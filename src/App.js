fe240fbd6c21f1d37df6d994db95bbd25da7d8c6
Rishav Chnada <rishavchanda0@gmail.com>
2022-04-20 21:13:46 +0530

    made some more changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
---
+++ src/components/Skills/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>Skills</div>
+  )
+}
+
+export default Skills
