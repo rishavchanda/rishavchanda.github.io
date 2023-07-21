b79efe78d96f0febcab515c9dd16ba81f477f264
rishavchanda <rishavchanda0@gmail.com>
2023-07-21 22:46:43 +0530

    Updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/rishavchanda",
   resume:
-    "https://drive.google.com/file/d/1Pu1Q9Z2wBG56mdwJoVkns78wZxxi3v4y/view?usp=sharing",
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/rishav_chanda/",
